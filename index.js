794d621bbfc7cf7e412bf9296a43772ce6ea09da
AxelChaikh <axelchaikh@gmail.com>
2024-11-18 15:11:04 -0300

    Swagger

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express"
 import usuariosRoutes from "./routes/usuario.route.js"
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerjsdoc from 'swagger-jsdoc'
+
+
+
+
 const PORT = 8080
 const app = express()
 
@@ -11,6 +17,23 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use("/", usuariosRoutes)
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        servers:[
+            {
+                url: "http://localhost:8080",
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+}
+
+const spacs = swaggerjsdoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spacs));
+
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
@@ -18,3 +41,8 @@ app.listen(PORT, () => {
 app.on("error", (err) => {
     console.log("Ha ocurrido un error en el servidor: ", err)
 })
+
+// Serve Swagger UI at `/api-docs`
+
+
+console.log('Swagger Docs available at http://localhost:8080/api-docs');
